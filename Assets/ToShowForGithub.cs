f1f091f789ca137e4f419f0eab8fc0b7e89275e1
zabbott <zabbott904@gmail.com>
2022-09-17 13:31:54 -0500

    Update ToShowForGithub.cs

--- Assets/ToShowForGithub.cs
+++ Assets/ToShowForGithub.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ToShowForGithub : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string PizzaString = "Pizza";
     void Start()
     {
         
